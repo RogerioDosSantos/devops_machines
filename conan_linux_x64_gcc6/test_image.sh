21a2b5830afe882526e955607e06fd261a32f8d3
Roger <roger.santos.main@gmail.com>
2018-10-12 16:18:02 -0500

    Added the conan_linux_x64_gcc6

---
+++ conan_linux_x64_gcc6/test_image.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./run_image.sh
+
+# docker exec conan_linux_x64_gcc6 sudo pip install -U conan_package_tools
+# docker exec conan_linux_x64_gcc6 sudo pip install -U conan
+docker exec conan_linux_x64_gcc6 conan user
+docker exec conan_linux_x64_gcc6 conan install gtest/1.8.0@bincrafters/stable -s arch=x86_64 -s compiler=gcc -s compiler.version=6 -s compiler.libcxx=libstdc++ --build
+
+./stop_image.sh
