af297b21fd905dc16fcd30123a51cdf1e6a142c6
Roger <roger.santos.main@gmail.com>
2018-12-12 11:54:59 -0600

    Improved Windows Core Debugger

--- windows_core_debugger/compile_image.sh
+++ windows_core_debugger/compile_image.sh
@@ -25,23 +25,9 @@ main::GetProxyConfig()
   echo "${proxy_config}"
 }
 
-main::PrepareStage()
-{
-  # local windows_10_x64_debuggers="/c/Program\ Files\ \(x86\)/Windows\ Kits/10/Debuggers/x64"
-  local windows_10_x64_debuggers='/c/Program Files (x86)/Windows Kits/10/Debuggers/x64/'
-  if [ ! -d "${windows_10_x64_debuggers}" ]; then
-    echo "Error: Cannot find Windows Debuggers for x64"
-    return 1
-  fi
-
-  echo "Copping Windows 10 (x64) debuggers (${windows_10_x64_debuggers})"
-  mkdir -p ./stage/windows_10_x64_debuggers
-  echo cp -r "${windows_10_x64_debuggers}" ./stage/windows_10_x64_debuggers
-  cp -r "${windows_10_x64_debuggers}" ./stage/windows_10_x64_debuggers
-}
-
-main::PrepareStage
-docker build -f ./os.docker $(main::GetProxyConfig) -t "rogersantos/windows_core_debugger:windows" .
-docker inspect rogersantos/windows_core_debugger:windows | grep flavor | head -1
+docker build -f ./os_stage.docker $(main::GetProxyConfig) -t "rogersantos/windows_core_debugger:os_stage" .
+docker build -f ./os.docker $(main::GetProxyConfig) -t "rogersantos/windows_core_debugger:os_windows" .
+docker build --no-cache -f ./app.docker $(main::GetProxyConfig) -t "rogersantos/windows_core_debugger:windows" .
+docker inspect rogersantos/windows_core_debugger:windows | grep uri | head -1
 
 popd > /dev/null
